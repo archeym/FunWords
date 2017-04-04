7eadc35864252a25f1c104d9042bdcf3a5550c4d
eve0770 <calvin.kl1026@gmail.com>
2017-04-04 14:18:47 +0800

    Initial Commit

---
+++ FunWords/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FunWords
+//
+//  Created by NEXTAcademy on 4/4/17.
+//  Copyright © 2017 ArchieApp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
